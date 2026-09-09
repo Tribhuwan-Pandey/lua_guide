918e3723a9e6646c332027ec49169003f243677a
Tribhuwan Pandey <tribhuwanpandeyrmr@gmail.com>
2026-09-09 08:02:43 +0530

    adding first basics
    lua_basics is been generated

---
+++ README.md
@@ -0,0 +1,5 @@
+# LUA_GUIDE
+
+This is a lua_guide which take you to from basics and advance
+Contents
+- lua_basics containing all basics
---
+++ lua basics/all_basics_of_lua.lua
@@ -0,0 +1,28 @@
+-- lua variables
+local varname= "hello"
+local var2=23
+local var3=2.3 
+-- lua is like pytho auto take the type of variable
+print("hello"..varname.."yooo"..var2.." this is like ".. var3) --printing in lua
+-- this is single comment 
+
+
+--example code
+local likes = 100
+print("your number of likes "..likes)
+
+--Datatype
+local string="this is a string"--string
+local bool=false --bool
+local list= {"heelo","ayo","yoo"}--table array
+local dict={age="hllo"}--table dict
+print(string..bool..list[2]..dict.age)
+--this is a example
+
+--if you come from background of python or c 
+--the indexing is generally form 0 but 
+-- in lua idexing is from 1 
+local list={'hello','you','good'}
+print(list[1])--this will print hello not you
+
+
