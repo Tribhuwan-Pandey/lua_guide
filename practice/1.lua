35bf12a916ebeaaee9b3c32109373ee996b9bad9
Tribhuwan Pandey <tribhuwanpandeyrmr@gmail.com>
2026-09-11 14:10:44 +0530

    2 problem solved

---
+++ practice/1.lua
@@ -0,0 +1,13 @@
+--
+--Write a Lua program that prints:
+
+--```text
+--Hello, Lua!
+--I am learning programming.
+--```
+local function fun1()
+print("Hello, Lua!")
+print("I am learning programming.")
+end
+
+fun1()
---
+++ practice/2.lua
@@ -0,0 +1,21 @@
+
+--[[
+*Concepts:** variables, strings, string concatenation
+
+Create variables for:
+
+- your name
+- your age
+- your college
+
+Print one sentence using all three.
+
+Example:
+
+```text
+My name is Alex, I am 20 years old, and I study at XYZ College.]]
+local name="name"
+local age=20
+local college="college name"
+print("My name is "..name..",I am "..age.."years old, and I study at "..college)
+
